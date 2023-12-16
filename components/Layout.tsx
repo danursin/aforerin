64a0a69bb01d8c5fb66c7091922996df0d4b06e3
danursin <danursin@gmail.com>
2023-12-16 14:52:52 -0600

    fix: remove nav links

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,6 @@
 import { Grid, Header, Menu } from "semantic-ui-react";
 
 import Head from "next/head";
-import Link from "next/link";
 import { ReactNode } from "react";
 import { ToastContainer } from "react-toastify";
 
@@ -19,15 +18,6 @@ const Layout: React.FC<LayoutProps> = ({ title = '"A" for Erin', children }) => 
             </Head>
             <Menu fluid tabular>
                 <Menu.Item content={<Header content={'"A" for Erin'} color="blue" icon="doctor" />} position="left" />
-                <Menu.Item>
-                    <Link href="/exam1">Exam 1</Link>
-                </Menu.Item>
-                <Menu.Item>
-                    <Link href="/exam2">Exam 2</Link>
-                </Menu.Item>
-                <Menu.Item>
-                    <Link href="/exam3">Exam 3</Link>
-                </Menu.Item>
             </Menu>
             <Grid padded="horizontally" centered>
                 <Grid.Column largeScreen={8} tablet={12} mobile={16}>
